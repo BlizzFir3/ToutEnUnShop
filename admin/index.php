446d6162627283bfe25bb7039448a1fc962e5695
BlizzFir3 <111078772+BlizzFir3@users.noreply.github.com>
2024-05-13 15:51:07 +0200

    Added security on admins pages
    cannot access without an admin login session

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,10 @@
 <?php
+	session_start();
+	if (!isset($_SESSION["zWupjTBoui6o91iNt"])){
+		header("Location: ../login.php");
+	} elseif (empty($_SESSION["zWupjTBoui6o91iNt"])){
+		header("Location: ../login.php");
+	}
 	require("../config/commandes.php");
 ?>
 
@@ -13,7 +19,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-	<script src="../color-modes.js"></script>
+	<script src="../assets/js/color-modes.js"></script>
 </head>
 <bodyclass="bg-body-tertiary">
   <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
--- login.php
+++ login.php
@@ -179,20 +179,8 @@
 <?php 
 /*if(isset($_POST['envoyer'])) {
 	if(!empty($_POST['email']) and !empty($_POST['mdp'])) {
-		// connexion à la base de données
-		$db_username 	= 'root';
-		$db_password 	= '';
-		$db_name 		= 'toutenunshop';
-		$db_host 		= 'localhost';
-		$access 		= mysqli_connect($db_host, $db_username, $db_password,$db_name)
-		or die('could not connect to database');
-		//	$email 			= htmlspecialchars($_POST['email']);
-		//	$mdp 			= htmlspecialchars($_POST['mdp']);
-		// on applique les deux fonctions mysqli_real_escape_string et htmlspecialchars
-		// pour éliminer toute attaque de type injection SQL et XSS
-		$email	= mysqli_real_escape_string($access,htmlspecialchars($_POST['email'])); 
-		$mdp	= mysqli_real_escape_string($access,htmlspecialchars($_POST['mdp']));
-		$admin	= getAdmin($email, $mdp);
+		$email 			= htmlspecialchars($_POST['email']);
+		$mdp 			= htmlspecialchars($_POST['mdp']);
 
 		if($admin){
 			$_SESSION['zWupjTBoui6o91iNt'] = $admin;
